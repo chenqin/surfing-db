14a55fc669f829b9e74bd33b28049b4b2c446105
Chen Q <cqin@pinterest.com>
2020-12-30 15:05:19 -1000

    add pg_query library

---
+++ include/main.h
@@ -0,0 +1,8 @@
+//
+// Created by Chen Qin on 12/30/20.
+//
+
+#ifndef SURFING_DB_MAIN_H
+#define SURFING_DB_MAIN_H
+
+#endif //SURFING_DB_MAIN_H
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Chen Qin on 12/30/20.
+//
+
+#include "../include/main.h"
+#include <iostream>
+#include <pg_query.h>
+int main() {
+    std::cout << "Hello World!";
+}
