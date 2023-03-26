d96de952f67a2bcd6192ca68f2120e2e25880657
Chen Qin <cqin@drsquirrel-pii-dev-0a018656.ec2.pin220.com>
2023-03-26 05:41:20 +0000

    [dr squirrel] port metrics and rawlog procesisng function logic over

--- surfing-db-java/src/main/java/MyBridge.java
+++ surfing-db-java/src/main/java/MyBridge.java
@@ -2,9 +2,19 @@ import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.arrow.c.ArrowArray;
+import org.apache.arrow.vector.types.pojo.Field;
+import org.apache.arrow.c.ArrowSchema;
+import org.apache.arrow.c.Data;
+import org.apache.arrow.memory.BufferAllocator;
+import org.apache.arrow.memory.RootAllocator;
 
 import com.pinterest.drsquirrel.parsers.DrSquirrelUtils;
 import com.pinterest.drsquirrel.parsers.FlinkMetricParser;
+import com.pinterest.drsquirrel.schema.FlinkJobInfo;
+import com.pinterest.drsquirrel.schema.FlinkMetric;
+import com.pinterest.drsquirrel.schema.FlinkMetricType;
+import com.pinterest.drsquirrel.schema.RawLog;
 import org.apache.arrow.vector.types.pojo.Field;
 import org.apache.arrow.vector.types.pojo.Schema;
 import org.apache.arrow.vector.BitVector;
@@ -13,6 +23,11 @@ import org.apache.arrow.vector.VarCharVector;
 import org.apache.arrow.vector.VectorSchemaRoot;
 import org.apache.arrow.vector.VarCharVector;
 import org.apache.arrow.vector.util.Text;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * my bridge example
@@ -26,34 +41,53 @@ public class MyBridge extends Bridge {
      */
     protected static VectorSchemaRoot process(VectorSchemaRoot input) throws Exception{
         total += input.getRowCount();
-        Schema schema = input.getSchema();
-        VarCharVector type = (VarCharVector) input.getVector("topic");
-        VarCharVector payload = (VarCharVector) input.getVector("payload");
+       
+        VarCharVector jobId = new VarCharVector("jobid", allocator);
+        jobId.allocateNew();
+        int count = 0;
+        jobId.setSafe(count++, new Text());
+       
+        VarCharVector input_type = (VarCharVector) input.getVector("topic");
+        VarCharVector input_payload = (VarCharVector) input.getVector("payload");
+        
         for(int i = 0 ; i < input.getRowCount(); i++) {
-            String topic = Text.decode(type.get(i));
+            String topic = Text.decode(input_type.get(i));
+            String body = Text.decode(input_payload.get(i));
             if(topic.equals("metric")) {
-
+                FlinkMetric flinkMetric = DrSquirrelUtils.constructFlinkMetric(body);
+                
+                if(flinkMetric.getJobId() != null
+                    && (flinkMetric.getType() == FlinkMetricType.JOBMANAGER_JOB
+                    || flinkMetric.getType() == FlinkMetricType.JOBMANAGER
+                    || flinkMetric.getType() == FlinkMetricType.JOBMANAGER_JOB_WITH_APPID
+                    || flinkMetric.getType() == FlinkMetricType.JOBMANAGER_WITH_APPID)) {
+                    
+                    //String json_str = mapper.writeValueAsString(flinkMetric);
+                    jobId.setSafe(count, flinkMetric.getJobId().getBytes(StandardCharsets.UTF_8));
+                    //json.setSafe(count, json_str.getBytes(StandardCharsets.UTF_8));
+                    count++;
+                }
+                
             } else if (topic.equals("log")) {
-
+                RawLog rawLog = DrSquirrelUtils.constructRawLog(body, "log");
+                //String json_str = mapper.writeValueAsString(rawLog);
+                //System.out.println(json_str);
+                
+                if(rawLog.getFile().contains("ExecutionGraph")) {
+                    //String json_str = mapper.writeValueAsString(rawLog);
+                    jobId.setSafe(count, rawLog.getApplicationId().getBytes(StandardCharsets.UTF_8));
+                    //json.setSafe(count, json_str.getBytes(StandardCharsets.UTF_8));
+                    count++;
+                }
+            } else {
+                throw new RuntimeException("topic is not supported");
             }
         }
 
-        //DrSquirrelUtils.constructFlinkMetric("");
-        //DrSquirrelUtils.constructFlinkMetric("");
-
-        BitVector bitVector = new BitVector("boolean", allocator);
-        VarCharVector varCharVector = new VarCharVector("varchar", allocator);
-        bitVector.allocateNew();
-        varCharVector.allocateNew();
-        for (int i = 0; i < 10; i++) {
-            bitVector.setSafe(i, i % 2 == 0 ? 0 : 1);
-            varCharVector.setSafe(i, ("test" + i).getBytes(StandardCharsets.UTF_8));
-        }
-        bitVector.setValueCount(10);
-        varCharVector.setValueCount(10);
-        List<Field> fields = Arrays.asList(bitVector.getField(), varCharVector.getField());
-        List<FieldVector> vectors = Arrays.asList(bitVector, varCharVector);
-        VectorSchemaRoot vectorSchemaRoot = new VectorSchemaRoot(fields, vectors);
+        jobId.setValueCount(count);
+        List<FieldVector> vectors = Arrays.asList(jobId);
+        VectorSchemaRoot vectorSchemaRoot = new VectorSchemaRoot(vectors);
+        vectorSchemaRoot.setRowCount(count);
         return vectorSchemaRoot;
     }
 }
