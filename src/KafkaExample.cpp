3bac532f4bfa2c9cceaf448b1d5864a04fc48c92
Chen Qin <qinnchen@gmail.com>
2023-04-03 15:04:53 -0700

    Update KafkaExample.cpp

--- src/KafkaExample.cpp
+++ src/KafkaExample.cpp
@@ -66,8 +66,8 @@ int main(int argc, char** argv) {
   /**
    * pull every 2 seconds
    */
-  int batch = 5000000;
-  int interval = 5000;
+  int batch = 50000;
+  int interval = 500;
   int world = node->world;
 
   size_t total = 0;
